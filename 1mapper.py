8cab124b4955abfa3396d6b04e2913caeff73a5c
Azhar Alali <46798680+AzharAlali@users.noreply.github.com>
2020-02-12 14:50:55 -0600

    editing

--- 1mapper.py
+++ 1mapper.py
@@ -1,10 +1,10 @@
-input = open("d.txt", "r")
-output = open("01.txt", "w")
+i = open("purchases.txt", "r")
+o = open("01.txt", "w")
 
-for line in input:
+for line in i:
     datalist = line.strip().split("    ")
     date, time,store, item, cost, paymentType = datalist
-    output.write(paymentType + "\t" + count + "\n")
+    o.write(paymentType + "\t" + "1" + "\n")
 
-input.close()
-output.close()
+i.close()
+o.close()
--- 3reducer.py
+++ 3reducer.py
@@ -2,27 +2,27 @@ s = open("02.txt","r")
 r = open("03.txt", "w")
 
 thisKey = ""
-thisValue = 0.0
-sum = 0.0
+thisValue = 0
+
 
 for line in s:
   data = line.strip().split('\t')
-  pymentType = data
+  pymentType, count = data
 
   if pymentType != thisKey:
     if thisKey:
       # output the last key value pair result
-      r.write(thisKey + '\t' + float(thisValue)+'\n')
+      r.write(thisKey + '\t' + str(thisValue)+'\n')
 
     # start over when changing keys
     thisKey = pymentType 
     thisValue = 0.0
   
   # apply the aggregation function
-  thisValue += float(sum)
+  thisValue += float(count)
 
 # output the final entry when done
-r.write(thisKey + '\t' + (thisValue)+'\n')
+r.write(thisKey + '\t' + str(thisValue)+'\n')
 
 s.close()
 r.close()
