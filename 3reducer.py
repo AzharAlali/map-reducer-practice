e21b0981670fdbb0640d9b0b5a7c6db51a621ea7
Azhar Alali <46798680+AzharAlali@users.noreply.github.com>
2020-02-12 14:28:26 -0600

    worked

--- 3reducer.py
+++ 3reducer.py
@@ -3,25 +3,26 @@ r = open("03.txt", "w")
 
 thisKey = ""
 thisValue = 0.0
+sum = 0.0
 
 for line in s:
   data = line.strip().split('\t')
-  store, amount = data
+  pymentType = data
 
-  if store != thisKey:
+  if pymentType != thisKey:
     if thisKey:
       # output the last key value pair result
-      r.write(thisKey + '\t' + str(thisValue)+'\n')
+      r.write(thisKey + '\t' + float(thisValue)+'\n')
 
     # start over when changing keys
-    thisKey = store 
+    thisKey = pymentType 
     thisValue = 0.0
   
   # apply the aggregation function
-  thisValue += float(amount)
+  thisValue += float(sum)
 
 # output the final entry when done
-r.write(thisKey + '\t' + str(thisValue)+'\n')
+r.write(thisKey + '\t' + (thisValue)+'\n')
 
 s.close()
 r.close()
